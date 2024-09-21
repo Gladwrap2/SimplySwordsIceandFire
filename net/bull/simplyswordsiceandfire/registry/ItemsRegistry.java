e145edadde0f80f3c9be980e89d54ab041fd547c
Gladwrap2 <64769815+Gladwrap2@users.noreply.github.com>
2024-09-21 06:17:30 -0500

    Update ItemsRegistry.java

--- net/bull/simplyswordsiceandfire/registry/ItemsRegistry.java
+++ net/bull/simplyswordsiceandfire/registry/ItemsRegistry.java
@@ -8,7 +8,7 @@ import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Rarity;
 import net.sweenus.simplyswords.SimplySwords;
-import net.sweenus.simplyswords.config.Config;
+import net.sweenus.simplyswordsiceandfire.config.Config;
 import net.sweenus.simplyswords.config.ConfigDefaultValues;
 import net.bull.simplyswordsiceandfire.item.*;
 
@@ -19,110 +19,110 @@ public class ItemsRegistry {
     public static final DeferredRegister<Item> ITEM = DeferredRegister.create(SimplySwordsIceandFire.MOD_ID, RegistryKeys.ITEM);
   
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_FIRE_TWINBLADE = ITEM.register( "dragonsteel_fire_twinblade", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + twinblade_positive_modifier - twinblade_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_FIRE,
+                    (int) (dragonsteel_modifier + twinblade_positive_modifier - twinblade_negative_modifier),
                     twinblade_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_FIRE_RAPIER = ITEM.register( "dragonsteel_fire_rapier", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + rapier_positive_modifier - rapier_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_FIRE,
+                    (int) (dragonsteel_modifier + rapier_positive_modifier - rapier_negative_modifier),
                     rapier_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_FIRE_KATANA = ITEM.register( "dragonsteel_fire_katana", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + katana_positive_modifier - katana_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_FIRE,
+                    (int) (dragonsteel_modifier + katana_positive_modifier - katana_negative_modifier),
                     katana_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_FIRE_SPEAR = ITEM.register( "dragonsteel_fire_spear", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + spear_positive_modifier - spear_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_FIRE,
+                    (int) (dragonsteel_modifier + spear_positive_modifier - spear_negative_modifier),
                     spear_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE)));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_FIRE_GLAIVE = ITEM.register( "dragonsteel_fire_glaive", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + glaive_positive_modifier - glaive_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_FIRE,
+                    (int) (dragonsteel_modifier + glaive_positive_modifier - glaive_negative_modifier),
                     glaive_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
   
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_FIRE_CLAYMORE = ITEM.register( "dragonsteel_fire_claymore", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + claymore_positive_modifier - claymore_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_FIRE,
+                    (int) (dragonsteel_modifier + claymore_positive_modifier - claymore_negative_modifier),
                     claymore_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_FIRE_GREATAXE = ITEM.register( "dragonsteel_fire_greataxe", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + greataxe_positive_modifier - greataxe_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_FIRE,
+                    (int) (dragonsteel_modifier + greataxe_positive_modifier - greataxe_negative_modifier),
                     greataxe_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_FIRE_SCYTHE = ITEM.register( "dragonsteel_fire_scythe", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + scythe_positive_modifier - scythe_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_FIRE,
+                    (int) (dragonsteel_modifier + scythe_positive_modifier - scythe_negative_modifier),
                     scythe_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_FIRE_HALBERD = ITEM.register( "dragonsteel_fire_halberd", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + halberd_positive_modifier - halberd_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_FIRE,
+                    (int) (dragonsteel_modifier + halberd_positive_modifier - halberd_negative_modifier),
                     halberd_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
   
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_ICE_TWINBLADE = ITEM.register( "dragonsteel_ice_twinblade", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + twinblade_positive_modifier - twinblade_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_ICE,
+                    (int) (dragonsteel_modifier + twinblade_positive_modifier - twinblade_negative_modifier),
                     twinblade_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_ICE_RAPIER = ITEM.register( "dragonsteel_ice_rapier", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + rapier_positive_modifier - rapier_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_ICE,
+                    (int) (dragonsteel_modifier + rapier_positive_modifier - rapier_negative_modifier),
                     rapier_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_ICE_KATANA = ITEM.register( "dragonsteel_ice_katana", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + katana_positive_modifier - katana_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_ICE,
+                    (int) (dragonsteel_modifier + katana_positive_modifier - katana_negative_modifier),
                     katana_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_ICE_SPEAR = ITEM.register( "dragonsteel_ice_spear", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + spear_positive_modifier - spear_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_ICE,
+                    (int) (dragonsteel_modifier + spear_positive_modifier - spear_negative_modifier),
                     spear_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE)));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_ICE_GLAIVE = ITEM.register( "dragonsteel_ice_glaive", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + glaive_positive_modifier - glaive_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_ICE,
+                    (int) (dragonsteel_modifier + glaive_positive_modifier - glaive_negative_modifier),
                     glaive_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
   
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_ICE_CLAYMORE = ITEM.register( "dragonsteel_ice_claymore", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + claymore_positive_modifier - claymore_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_ICE,
+                    (int) (dragonsteel_modifier + claymore_positive_modifier - claymore_negative_modifier),
                     claymore_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_ICE_GREATAXE = ITEM.register( "dragonsteel_ice_greataxe", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + greataxe_positive_modifier - greataxe_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_ICE,
+                    (int) (dragonsteel_modifier + greataxe_positive_modifier - greataxe_negative_modifier),
                     greataxe_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_ICE_SCYTHE = ITEM.register( "dragonsteel_ice_scythe", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + scythe_positive_modifier - scythe_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_ICE,
+                    (int) (dragonsteel_modifier + scythe_positive_modifier - scythe_negative_modifier),
                     scythe_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
 
     public static final RegistrySupplier<DragonsteelSwordItem> DRAGONSTEEL_ICE_HALBERD = ITEM.register( "dragonsteel_ice_halberd", () ->
-            new DragonsteelSwordItem(ModToolMaterial.RUNIC,
-                    (int) (runic_modifier + halberd_positive_modifier - halberd_negative_modifier),
+            new DragonsteelSwordItem(ModToolMaterial.DRAGONSTEEL_ICE,
+                    (int) (dragonsteel_modifier + halberd_positive_modifier - halberd_negative_modifier),
                     halberd_attackspeed,
                     new Item.Settings().arch$tab(SimplySwords.SIMPLYSWORDS).rarity(Rarity.RARE).fireproof()));
 
